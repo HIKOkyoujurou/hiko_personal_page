49753848d151b322da4058ebe0579c2c7252b225
HikoKyoujurou <hikokyoujurou@gmail.com>
2026-04-06 20:01:35 +0900

    あ

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/personal_page/",
+  base: "/hiko_personal_page/",
   plugins: [react()],
 });
